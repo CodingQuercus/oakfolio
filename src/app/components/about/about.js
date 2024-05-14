86572b37a7855820623c55acf95f16ffff2a4614
CodingQuercus <123453349+CodingQuercus@users.noreply.github.com>
2024-05-15 00:00:14 +0200

    fix

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -31,7 +31,7 @@ const About = () => {
             </div>
             <div className={styles.buttonContainer}>
                 <p className={styles.buttonText}>Download my CV:</p>
-                <button className={styles.button} onClick={handleDownload}>
+                <button className={styles.cvButton} onClick={handleDownload}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
                         <path d="M15 22.7646L5.625 13.3896L8.25 10.6709L13.125 15.5459V0.264648H16.875V15.5459L21.75 10.6709L24.375 13.3896L15 22.7646ZM3.75 30.2646C2.71875 30.2646 1.83594 29.8975 1.10156 29.1631C0.367188 28.4287 0 27.5459 0 26.5146V20.8896H3.75V26.5146H26.25V20.8896H30V26.5146C30 27.5459 29.6328 28.4287 28.8984 29.1631C28.1641 29.8975 27.2813 30.2646 26.25 30.2646H3.75Z" fill="#F3F3F3" />
                     </svg>
